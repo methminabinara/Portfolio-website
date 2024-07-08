cedf0775d17cf14756f0e5542fcef86092fcdaac
Methmina Binara <binaraepa@gmail.com>
2024-07-08 10:52:55 +0530

    feat: Created toggle icon for navbar

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,9 @@
-let menuIcon = document.querySelector('#menu-icon')
+/*=========================== toggle icon navbar  ===========================*/
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark')
+    navbar.classList.toggle('active')
+}

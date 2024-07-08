5f3aa668515c68f765f74c2c3bdad88f628d7ebe
Methmina Binara <binaraepa@gmail.com>
2024-07-08 20:54:00 +0530

    feat: Set up Typed.js

--- JS/main.js
+++ JS/main.js
@@ -51,4 +51,14 @@ window.onscroll = () => {
     ScrollReveal().reveal('.home-img, services-container, portfolio-box, .contact form', { origin: 'bottom' });
     ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left' });
     ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right' });
+
+    /*=========================== typed js  ===========================*/
+
+    const typed = new Typed('.multiple-text', {
+        strings: ['Frontend Developer', 'Web Designer', 'Youtuber'],
+        typeSpeed: 70,
+        backSpeed: 70,
+        backDelay: 1000,
+        loop: true,
+    });
 }

00c4a8596f5ce0ac604371f3dadd5247413012d7
Methmina Binara <binaraepa@gmail.com>
2024-07-08 16:03:54 +0530

    WIP

--- JS/main.js
+++ JS/main.js
@@ -38,4 +38,15 @@ window.onscroll = () => {
 
     menuIcon.classList.remove('fa-xmark');
     navbar.classList.remove('active');
+
+    /*=========================== scroll reveal  ===========================*/
+
+    ScrollReveal({
+        distance: '80px',
+        duration: 2000,
+        delay: 200,
+    });
+
+    ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
+    ScrollReveal().reveal('.home-img, services-container, portfolio-box, .contact form', { origin: 'buttom' });
 }
